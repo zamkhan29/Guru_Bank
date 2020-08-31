e7133ccf1c7415386004b77a2ec93f27b91ef42f
Zamir <zameerkhan29@gmail.com>
2020-08-31 23:43:08 +0530

    edge driver added

--- src/test/java/com/qa/test/LoginPageTest.java
+++ src/test/java/com/qa/test/LoginPageTest.java
@@ -1,7 +1,6 @@
 
 package com.qa.test;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 import com.qa.pages.LoginPage;
--- src/test/java/com/qa/test/TestBase.java
+++ src/test/java/com/qa/test/TestBase.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -29,8 +30,8 @@ public class TestBase{
 	
 	@BeforeMethod
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
-		driver = new ChromeDriver();
+		System.setProperty("webdriver.edge.driver","./driver/msedgedriver.exe");
+		driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
