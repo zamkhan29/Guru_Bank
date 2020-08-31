44f637557463c22a15741099e57c6c38d35c1eec
Zamir <zameerkhan29@gmail.com>
2020-08-31 21:36:58 +0530

    minor changes in code

--- src/test/java/com/qa/test/TestBase.java
+++ src/test/java/com/qa/test/TestBase.java
@@ -33,6 +33,7 @@ public class TestBase{
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(prop.getProperty("Url"));
 		
